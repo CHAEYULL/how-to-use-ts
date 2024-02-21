8054542d52a59077bec7ee726f29ac62a28a0b76
leo <leopportunity@gmail.com>
2024-02-21 16:06:16 +0900

    폼 전송 실습 완료

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 "use strict";
 var navBtn = document.querySelector('.navbar-toggler');
 var navList = document.querySelector('.list-group');
+var loginBtn = document.querySelector('#login');
+var loginCloseBtn = document.querySelector('#close');
+var loginForm = document.querySelector('.black-bg');
+var loginID = document.querySelector('#id');
+var loginPassword = document.querySelector('#password');
+var submitBtn = document.querySelector('form');
+var image1 = document.querySelector('#btn1');
+var image2 = document.querySelector('#btn2');
+var image3 = document.querySelector('#btn3');
 if (navBtn instanceof HTMLElement) {
     navBtn.addEventListener('click', function () {
         if (navList instanceof HTMLElement) {
-            navList.classList.toggle('display-none');
+            navList.classList.toggle('list-group-ani');
+        }
+    });
+}
+if (loginBtn instanceof HTMLElement) {
+    loginBtn.addEventListener('click', function () {
+        if (loginForm instanceof HTMLElement) {
+            loginForm.classList.add('display-show');
+        }
+    });
+}
+if (loginCloseBtn instanceof HTMLElement) {
+    loginCloseBtn.addEventListener('click', function () {
+        loginForm === null || loginForm === void 0 ? void 0 : loginForm.classList.remove('display-show');
+    });
+}
+if (submitBtn instanceof HTMLElement) {
+    submitBtn.addEventListener('submit', function (e) {
+        if (loginID instanceof HTMLInputElement && loginPassword instanceof HTMLInputElement) {
+            if (loginID.value.trim() == '' || loginPassword.value.trim() == '') {
+                e.preventDefault();
+                alert('공백은 안돼.');
+                loginID.value == '';
+                loginPassword.value == '';
+            }
         }
     });
 }
--- index.ts
+++ index.ts
@@ -1,10 +1,51 @@
 let navBtn: HTMLElement | null = document.querySelector('.navbar-toggler');
 let navList: HTMLElement | null = document.querySelector('.list-group');
+let loginBtn : HTMLElement | null = document.querySelector('#login')
+let loginCloseBtn: HTMLElement |  null = document.querySelector('#close')
+let loginForm: HTMLElement | null = document.querySelector('.black-bg')
+let loginID: HTMLInputElement | null = document.querySelector('#id')
+let loginPassword: HTMLInputElement | null = document.querySelector('#password')
+let submitBtn:HTMLElement | null = document.querySelector('form')
+let image1:HTMLElement | null = document.querySelector('#btn1')
+let image2:HTMLElement | null = document.querySelector('#btn2')
+let image3:HTMLElement | null = document.querySelector('#btn3')
+
+
+
+
+
 
 if ( navBtn instanceof HTMLElement){
     navBtn.addEventListener('click', function(){
         if ( navList instanceof HTMLElement){
-            navList.classList.toggle('display-none')
+            navList.classList.toggle('list-group-ani')
+        }
+    })
+}
+
+if (loginBtn instanceof HTMLElement){
+    loginBtn.addEventListener('click',function(){
+        if (loginForm instanceof HTMLElement){
+            loginForm.classList.add('display-show')
+        }
+    })
+}
+
+if (loginCloseBtn instanceof HTMLElement){
+    loginCloseBtn.addEventListener('click',function(){
+        loginForm?.classList.remove('display-show')
+    })
+}
+
+if (submitBtn instanceof HTMLElement){
+    submitBtn.addEventListener('submit', function(e){
+        if (loginID instanceof HTMLInputElement && loginPassword instanceof HTMLInputElement){
+            if ( loginID.value.trim() == '' || loginPassword.value.trim() == ''){
+                e.preventDefault()
+                alert('공백은 안돼.')
+                loginID.value == '';
+                loginPassword.value == '';
+            }
         }
     })
 }
